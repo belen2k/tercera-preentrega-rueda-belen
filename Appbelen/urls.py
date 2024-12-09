173a8c3649eaaa486e35365036934ecc5e8aa1f6
Nico2k1 <nikolas5766@gmail.com>
2024-12-09 14:15:22 -0300

    proyecto final 1

--- Appbelen/urls.py
+++ Appbelen/urls.py
@@ -9,3 +9,10 @@ urlpatterns = [
     path('inicio/',views.inicio,name='inicio' ),
      path('buscar_estudiantes/',views.buscar_estudiantes,name='buscar_estudiantes' ),
 ]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('signup/, views.signup', name='signup'),
+     path('tasks/, tasks.signup', name='tasks')
+]
--- Appbelen/views.py
+++ Appbelen/views.py
@@ -1,8 +1,12 @@
 
 # Create your views here.
 from django.shortcuts import render, redirect
-from .models import Profesor , Nota , Estudiante
+from django.contrib.auth.models import UserCreationForm, AuthenticationForm
+from .models import  Profesor , Nota , Estudiante
 from .forms import ProfesorForm, NotaForm, EstudianteForm
+from django.contrib.auth.models import user 
+from django.http import HttpResponse
+from django.contrib.auth import login, logout, authenticate
 
 def agregar_profesor(request):
     if request.method == 'POST':
@@ -56,3 +60,53 @@ def buscar_estudiantes(request):
 
     # Renderizar la plantilla y enviar las universidades y la consulta como contexto.
     return render(request, 'appbelen/buscar_estudiante.html', {'estudiantes': estudiantes, 'query': query})
+
+
+def home(request):
+    return render (request, 'home.html')
+
+def signup(request):
+   if request.method =='GET':
+       return render (request, 'signup.html' {
+  'form': UserCreationForm
+   })
+   else:
+       if request.POST['password1'] == request.POST['password2']:
+           user = user.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+           user.save()
+           login(request, user)
+           return redirect('tasks')
+           return HttpResponse('Usuario creado satisfactoriamente')
+
+
+def tasks(request):
+    return render(request,'tasks.html')
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+def signin(request):
+   if request.method=='GET':
+        return render (request, 'signin.html', {
+        'form' : AuthenticationForm
+    })
+   else:
+     user=authenticate(request, username=request.POST['username'], password=request.POST
+                     ['password'])
+     if user is none:
+          return render (request, 'signin.html', {
+         'form' : AuthenticationForm
+         'error':'username o password es incorrecto '
+    })
+     else:
+        login(request, user)
+        return redirect('tasks')
+
+
+
+     
+       
+      
+
+
